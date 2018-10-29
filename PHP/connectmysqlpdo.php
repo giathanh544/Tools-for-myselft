06963efe65d0118607b0fc5c905364f6fcd8535f
ThanhTrịnh <virussky99@gmail.com>
2018-10-29 21:57:05 +0700

    Connect mysql by PDO

---
+++ PHP/connectmysqlpdo.php
@@ -0,0 +1,28 @@
+<?php
+
+class DB{
+	private $servername;
+	private $username;
+	private $password;
+	private $dbname;
+	private $charset;
+
+	public function connect(){
+	$this->servername = "localhost";
+	$this->username = "root";
+	$this->password = "";
+	$this->dbname = "test";
+	$this->charset = "utf8";
+
+		try{
+			$dsn = "mysql:host=".$this->servername.";dbname=".$this->dbname.";charset=".$this->charset;
+			$pdo = new PDO($dsn, $this->username, $this->password);
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $pdo; 
+		}catch (PDOException $e)
+		{
+			echo "Connection failed: ".$e->getMessage();
+		}
+	}
+}
+ ?>
